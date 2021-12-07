993f0e9555cd511d241a942ec1f0a84c798aef6d
Carine Neris <carine_neris@hotmail.com>
2021-12-07 00:32:22 -0300

    rotas

--- amigo_certo/urls.py
+++ amigo_certo/urls.py
@@ -1,13 +1,15 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import UsuariosViewSet, ListaUsuarios, AcompanhanteViewSet
+from .views import  UsersViewSet 
 
 
 router = routers.DefaultRouter()
-router.register('cadastrar_usuario', UsuariosViewSet)
-router.register('cadastrar_acompanhante', AcompanhanteViewSet)
+router.register('cadastrar_usuario', UsersViewSet)
+# router.register('cadastrar_acompanhante', AcompanhanteViewSet, base_name="cadastro-acompanhante")
+# router.register('cadastrar_cliente', ClienteViewSet, base_name="cadastro-cliente")
+# router.register('lista_necessidades', ListNecessidadesViewSet, base_name="necessidades")
+# router.register('cadastrar_perfil', PerfilViewSet, base_name="cadastro-perfil")
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('cadastrar_usuario/', ListaUsuarios.as_view())
 ]
